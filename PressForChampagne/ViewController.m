2b9073ab3a399d327dc7cdef0732cde40d98e993
Jim Speth <jim@dwnld.me>
2016-01-08 16:18:10 -0500

    Tap and hold to edit message.

--- PressForChampagne/ViewController.m
+++ PressForChampagne/ViewController.m
@@ -9,7 +9,13 @@
 #import <Parse/Parse.h>
 #import "ViewController.h"
 
+@interface ViewController ()
+@property (nonatomic, strong) NSString *message;
+@end
+
 @implementation ViewController
+
+@synthesize message = _message;
 
 - (void)viewDidLoad
 {
@@ -17,20 +23,65 @@
     
     self.imageView.userInteractionEnabled = YES;
     
-    UITapGestureRecognizer *recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(requestChampagne)];
-    [self.imageView addGestureRecognizer:recognizer];
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(requestChampagne)];
+    [self.imageView addGestureRecognizer:tapRecognizer];
+    
+    UILongPressGestureRecognizer *longPressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(changeMessage:)];
+    [self.imageView addGestureRecognizer:longPressRecognizer];
 }
+
+#pragma mark - Properties
+
+#define kMessageKey @"PFCMessage"
+
+- (NSString *)message
+{
+    if (!_message.length) {
+        _message = [[NSUserDefaults standardUserDefaults] stringForKey:kMessageKey];
+        if (!_message.length) {
+            _message = @"More Champagne, please!";
+        }
+    }
+    return _message;
+}
+
+- (void)setMessage:(NSString *)message
+{
+    _message = message;
+    [[NSUserDefaults standardUserDefaults] setObject:_message forKey:kMessageKey];
+}
+
+#pragma mark - Actions
 
 - (void)requestChampagne
 {
     PFPush *push = [PFPush new];
-    [push setMessage:@"More Champagne, please!"];
+    [push setMessage:self.message];
     [push sendPushInBackground];
     
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Champagne Requested"
                                                                    message:nil
                                                             preferredStyle:UIAlertControllerStyleAlert];
     [alert addAction:[UIAlertAction actionWithTitle:@"Great!" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {}]];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)changeMessage:(UILongPressGestureRecognizer *)sender
+{
+    if (sender.state != UIGestureRecognizerStateBegan) {
+        return;
+    }
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Change Message"
+                                                                   message:nil
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.text = self.message;
+    }];
+    [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {}]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"Change" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        self.message = alert.textFields.firstObject.text;
+    }]];
     [self presentViewController:alert animated:YES completion:nil];
 }
 
